181fa3cb481e99d0d75ecbc48cc64027755da634
DenD <DenD@DESKTOP-DC1JO09>
2023-06-08 16:32:01 +0200

    Creating DTOs for Message model

---
+++ API/Infrastructure/DTOs/Message/CreateMessageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.DTOs.Message
+{
+    public class CreateMessageDto
+    {
+        public int ProjectId { get; set; }
+        public int SenderId { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ API/Infrastructure/DTOs/Message/MessageDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Infrastructure.DTOs.Message
+{
+    public class MessageDto
+    {
+        public int Id { get; set; }
+        public int ProjectId { get; set; } 
+        public int SenderId { get; set; }
+        public string SenderUsername { get; set; }
+        public string? SenderPhotoUrl { get; set; }
+        public string Content { get; set; }
+        public DateTime MessageSent { get; set; }
+    }
+}
